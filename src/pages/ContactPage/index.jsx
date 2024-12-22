4b2e408460b597821c39ed1aee370f1aff3918b5
Arnas <arnasbalciunas@gmail.com>
2024-12-22 20:33:28 +0100

    add contactPage

---
+++ src/pages/ContactPage/index.jsx
@@ -0,0 +1,9 @@
+import ContactForm from "../../components/ContactForm";
+
+// Page to display the Contact Form
+const ContactPage = () => {
+  document.title = "Contact Us | Shop";
+  return <ContactForm />;
+};
+
+export default ContactPage;
