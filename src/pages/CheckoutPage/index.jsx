ec0ed36e0dd530f61abe205fc93440b73de7a0a1
Arnas <arnasbalciunas@gmail.com>
2024-12-22 20:36:58 +0100

    ChekcoutPage

---
+++ src/pages/CheckoutPage/index.jsx
@@ -0,0 +1,9 @@
+import Checkout from "../../components/Checkout";
+
+// Page to display the Checkout
+const CheckoutPage = () => {
+  document.title = "Checkout | Shop";
+  return <Checkout />;
+};
+
+export default CheckoutPage;
