28e8aff23a34a5c1f6711d12038f88e2eea278cb
Arnas <arnasbalciunas@gmail.com>
2024-12-22 20:37:16 +0100

    CheckoutSuccessPage

---
+++ src/pages/CheckoutSuccessPage/index.jsx
@@ -0,0 +1,9 @@
+import CheckoutSuccess from "../../components/Checkout/CheckoutSuccess";
+
+// Page to display the Checkout Success
+const CheckoutSuccessPage = () => {
+  document.title = "Successful Order | Shop";
+  return <CheckoutSuccess />;
+};
+
+export default CheckoutSuccessPage;
